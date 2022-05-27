50ba17940d46789fc46f0a97802baba2a8971466
mbagas <karab4946@gmail.com>
2022-05-27 19:12:38 +0700

    fix: buggg

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="">
-    <link href="https:cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="http://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="http://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="http://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="http://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="index.js"></script>
 
 
 </head>
+
 <body>
     <div class="container">
         <h4 align="center">Pemesanan Produk</h4>
@@ -20,7 +27,7 @@
         <ul id="data">
 
         </ul>
-        
+
 
         <div class="row justify-content-md-center">
             <div class="col-6 border">
@@ -48,7 +55,7 @@
                     </div>
                     <div class="row g-3 input-group mb-3" id="inputFormRow">
                         <div class="col">
-                            <select class="form-select form-control coba" onchange="tes()" id="produk" name="produk" >
+                            <select class="form-select form-control coba" onchange="tes()" id="produk" name="produk">
                                 <option value="">Pilih Produk</option>
                                 <option value="Jet Tempur">Jet Tempur</option>
                                 <option value="Nuklir Hiroshima">Nuklir Hiroshima</option>
@@ -59,31 +66,19 @@
                         </div>
                         <div class="col">
                             <input type="number" name="jumlah" id="jumlah" class="form-control" placeholder="Last name" aria-label="Last name" required>
-                        </div>   
+                        </div>
                     </div>
                     <div id="newrow"></div>
                     <div class="row g-3">
                         <button id="hasil" onclick="tes2()" type="button" class="btn btn-primary">Pesan</button>
-                        
+
                     </div>
                 </form>
             </div>
         </div>
     </div>
-    
+
 </body>
 
-<script type="text/javascript"  src="https:code.jquery.com/jquery-3.5.0.js"></script>
-<script type="text/javascript"  src="https:ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script type="text/javascript"  src="https:cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-    crossorigin="anonymous"></script>
-<script type="text/javascript"  src="https:cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
-    crossorigin="anonymous"></script>
-<script type="text/javascript"  src="https:cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"></script>
-<script type="text/javascript" src="index.js"></script>
 
 </html>
